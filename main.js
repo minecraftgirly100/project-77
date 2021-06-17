00a37942a53b6c0d8f28dba572ec7d607c135910
minecraftgirly100 <81394175+minecraftgirly100@users.noreply.github.com>
2021-06-17 19:36:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+var first_name = "Serina";
+var last_name  = "keshwani";
+
+var full_name  = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b ;
+console.log(add);
+
+var sub = a - b ;
+console.log(sub);
+
+var multiplie = a * b ;
+console.log(multiplie);
+
+var divide = a / b ;
+console.log(divide);
